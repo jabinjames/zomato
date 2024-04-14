9625d762a396a0d76f9d6b074606c59e27e12e37
jabin <jabinjames2002@gmail.com>
2024-04-15 00:20:03 +0530

    third commit

--- src/components/Add_Restaurants/Header/header.jsx
+++ src/components/Add_Restaurants/Header/header.jsx
@@ -4,6 +4,10 @@ import Banner from '../../../Images/banners/banner2.jpg';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Login from '../../Reusable/compoenent3/login/Login';
 import Sign from '../../Reusable/compoenent3/signup/sign';
+import Glob from '../../../Images/icons/glob.png'
+import CalenderIcons from '../../../Images/icons/calenderIcon.png'
+import ResturantIcon from '../../../Images/icons/restaurantIcon.png'
+
 function Header() {
     return (
         <Router>
@@ -18,12 +22,46 @@ function Header() {
                         </ul>
                     </nav>
                 </div>
-                <div className="banner-container">
-                    <img src={Banner} alt="banner_image" />
-                </div>
                 {/* <Route path="http://localhost:5173"  /> */}
                 <Route path="/Login" component={Login} />
                 <Route path="/Sign" component={Sign} />
+                <div className="Resgister-title">
+                    <h2>Register your resturant on zomato </h2>
+                    <p>For free and get more customers</p>
+                    <div className="button-box">
+                        <div className="first-button">
+                            <button>Resgister your resturants</button>
+                        </div>
+                        <div className="second-button">
+                            <button>Resturant already listed? claim now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="second-header">
+                <div className="heading">
+                    <h2>Why should partner with Somato</h2>
+                    <p>Somato enables you to get 60% more renvenue,10x new customer and boost your  brand visiblity  by providing insights to improve your bussiness</p>
+                </div>
+            </div>
+            <div className="three-box">
+                <div className='outer-three-box'>
+                    <div className="first-box">
+                        <img src={Glob} alt='earths image' />
+                        <p className='first-para'>500+ cities</p>
+                        <p>in India</p>
+                    </div>
+                    <div className="second-box">
+                        <img src={CalenderIcons} alt="calenderIcon" />
+                        <p className='first-para'>2.5 lakhs+</p>
+                        <p>resturants listings</p>
+                    </div>
+                    <div className="third-box">
+                        <img src={ResturantIcon} alt="returants image" />
+                        <p className='first-para'>4 crore+</p>
+                        <p>monthly orders</p>
+                    </div>
+                </div>
             </div>
         </Router>
     );
